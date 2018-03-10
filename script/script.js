49421137a381ebc6b48e19c4a049c85686858724
Ervan Adetya <vaanz.adetya@gmail.com>
2018-03-10 22:54:46 +0700

    Detail finsihed

--- script/script.js
+++ script/script.js
@@ -4,12 +4,13 @@ function category(str) {
 }
 
 function home() {
-    localStorage.removeItem('category');
     window.location.href = "index.html";
 }
 
-function detail(obj) {
-    
+function detail(name, photo) {
+    localStorage.setItem('itemName', name);
+    localStorage.setItem('itemPhoto', photo);
+    window.location.href="detail.html"
 }
 
 function categoryGenerator() {
@@ -17,7 +18,6 @@ function categoryGenerator() {
     if(localStorage.getItem('category') == '3D Cake') obj = cake3D;
     else if(localStorage.getItem('category') == 'Butter Cream Cake') obj = butterCream;
     else if(localStorage.getItem('category') == 'Korean Flower Cake') obj = korean;
-
     let description = `
         <p class="title">${obj.title}</p>
         <p>${obj.description}</p>
@@ -28,7 +28,7 @@ function categoryGenerator() {
     $('.typeSection').append(rowTemplate);
     obj.item.forEach((cake) => {
         let template =`
-                    <a href='#' class="itemBox" onclick="category('3D Cake')">
+                    <a href='#' class="itemBox" onclick="detail('${cake.name}', '${cake.photo}')">
                         <div class="col s2">
                             <div class="red lighten-1">
                                 <img class="itemImg"
@@ -41,6 +41,23 @@ function categoryGenerator() {
                       `
         $('.row').append(template);
     })
+}
+
+function detailGenerator() {
+    let template = `
+        <img class="itemImg"
+            src="${localStorage.getItem('itemPhoto')}"
+            alt="https://lh6.googleusercontent.com/XNbKPWjdNzNgeg_bCpSoakFc20s9WW3R9s_NFiUiAnQFASj6a0yTKlwVcEKt5UIy3NbILke9JCfp2aY=w1366-h629-rw">
+        <p>${localStorage.getItem('itemName')}</p>
+        `
+    $('.image').append(template);
+
+    let description= `
+        <p>Name: ${localStorage.getItem('itemName')}</p>
+        <p>Category: ${localStorage.getItem('category')}</p>
+        <p>Description: ${lorem}</p>
+    `
+    $('.desc').append(description)
 }
 
 const item = {
@@ -65,3 +82,11 @@ const korean = {
     description: 'Inspired by korean flower.',
     item: [item,item,item,item,item,item,item,item,item,item,item,item]
 }
+
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
+
+let lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget lobortis ligula. Aliquam luctus sem et neque vulputate, sed elementum nibh faucibus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse potenti. Suspendisse tempus semper felis, sed porta erat tristique ut. Phasellus lectus sem, consectetur euismod lacus id, malesuada vestibulum massa. Nunc sagittis viverra mauris, nec sagittis nibh aliquet quis. Ut leo erat, posuere nec ligula non, commodo convallis lorem. Proin ornare at turpis eget semper.
+
+Morbi id nulla dapibus, ultricies metus id, vulputate purus. Morbi sagittis ornare commodo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi pretium et quam eu ullamcorper. Mauris suscipit lectus nulla, sit amet pulvinar libero venenatis nec. Nullam ultricies non nunc finibus sodales. Nullam auctor bibendum tortor ac vehicula. Praesent ultrices fermentum justo, et malesuada sapien volutpat non. Maecenas suscipit, est ac tincidunt scelerisque, nibh arcu rhoncus elit, vehicula egestas diam dui sit amet massa. Proin sapien turpis, lacinia sed nisl et, congue ultrices tellus. Phasellus volutpat, massa vitae pulvinar porta, odio ante viverra odio, id porta felis erat in diam. Fusce quis urna ultrices tellus posuere elementum non eget mi. Praesent vulputate, metus sit amet sagittis venenatis, leo nisl fermentum dui, quis rutrum mauris lacus vitae nisl. Aenean viverra vel sem pellentesque feugiat. Proin auctor dignissim nisi, at elementum lorem porta sit amet. Morbi in tincidunt neque, eget condimentum felis.`
